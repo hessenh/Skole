12d1b9743d075893424f965fafcaa955b84a1c48
Stian Hvatum <stian@dream-web.no>
2011-10-31 11:49:39 +0100

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   Eclipse/.metadata/.log
    	modified:   Eclipse/.metadata/.plugins/net.sf.eclipsensis/net.sf.eclipsensis.help.NSISPluginManager.Plugins.ser
    	modified:   Eclipse/.metadata/.plugins/org.eclipse.core.resources/.projects/TDT4186 - Oving 3/.indexes/e4/history.index
    	modified:   Eclipse/.metadata/.plugins/org.eclipse.core.resources/.safetable/org.eclipse.core.resources
    	modified:   Eclipse/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.debug.core.prefs
    	modified:   Eclipse/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.debug.ui.prefs
    	modified:   Eclipse/.metadata/.plugins/org.eclipse.dltk.core.index.sql.h2/model.trace.db
    	modified:   Eclipse/.metadata/.plugins/org.eclipse.jdt.core/279750358.index
    	modified:   Eclipse/.metadata/.plugins/org.eclipse.jdt.core/externalLibsTimeStamps
    	modified:   Eclipse/.metadata/.plugins/org.eclipse.jdt.core/savedIndexNames.txt
    	modified:   Eclipse/.metadata/.plugins/org.eclipse.wst.jsdt.core/externalLibsTimeStamps
    	modified:   Eclipse/.metadata/.plugins/org.maven.ide.eclipse/nexus/1bf6257a8b28e2b3e61019119379ee26/segments.gen
    	modified:   Eclipse/.metadata/.plugins/org.maven.ide.eclipse/nexus/830bc118332e77292949ed1e6d2fabe0/segments.gen
    	modified:   Eclipse/.metadata/.plugins/org.maven.ide.eclipse/nexus/d9d714e11cb097b3ffcec91cccc65d3e/segments.gen
    	modified:   Eclipse/TDT4186 - Oving 3/.project
    	new file:   Eclipse/TDT4186 - Oving 3/.~lock.results.ods#
    	modified:   Eclipse/TDT4186 - Oving 3/bin/CPU.class
    	modified:   Eclipse/TDT4186 - Oving 3/bin/Event.class
    	new file:   Eclipse/TDT4186 - Oving 3/bin/IO.class
    	modified:   Eclipse/TDT4186 - Oving 3/bin/Process.class
    	modified:   Eclipse/TDT4186 - Oving 3/bin/Simulator.class
    	modified:   Eclipse/TDT4186 - Oving 3/bin/Statistics.class
    	new file:   Eclipse/TDT4186 - Oving 3/results.ods
    	modified:   Eclipse/TDT4186 - Oving 3/src/CPU.java
    	modified:   Eclipse/TDT4186 - Oving 3/src/Event.java
    	new file:   Eclipse/TDT4186 - Oving 3/src/IO.java
    	modified:   Eclipse/TDT4186 - Oving 3/src/Process.java
    	modified:   Eclipse/TDT4186 - Oving 3/src/Simulator.java
    	modified:   Eclipse/TDT4186 - Oving 3/src/Statistics.java
    
     Changes not staged for commit:
       (use "git add/rm <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
       (commit or discard the untracked or modified content in submodules)
    
    	deleted:    Eclipse/.metadata/.plugins/org.eclipse.core.resources/.projects/TDT4186 - Oving 3/org.eclipse.jdt.core/state.dat
    	modified:   Eclipse/logres (modified content)

--- Eclipse/TDT4186 - Oving 3/src/CPU.java
+++ Eclipse/TDT4186 - Oving 3/src/CPU.java
@@ -8,39 +8,54 @@ public class CPU {
 	/** The amount of free memory in the memory device */
 	private long freeMemory;
 	private Process currentProcess;
-	
-	private final EventQueue eventQueue;
 
-	public CPU(Queue cpuQueue, long maxCpuTime, Statistics statistics, EventQueue eventQueue) {
+	private final EventQueue eventQueue;
+	private final Gui gui;
+
+	public CPU(Queue cpuQueue, long maxCpuTime, Statistics statistics,
+			EventQueue eventQueue, Gui gui) {
 		this.cpuQueue = cpuQueue;
 		this.maxCpuTime = maxCpuTime;
 		this.statistics = statistics;
 		this.eventQueue = eventQueue;
+		this.gui = gui;
 	}
 
 	public void timePassed(long timePassed) {
 		statistics.cpuQueueLengthTime += cpuQueue.getQueueLength() * timePassed;
-		if (cpuQueue.getQueueLength() > statistics.memoryQueueLargestLength) {
+		if (cpuQueue.getQueueLength() > statistics.cpuQueueLargestLength) {
 			statistics.cpuQueueLargestLength = cpuQueue.getQueueLength();
 		}
 	}
 
-	public void insertProcess(Process p) {
+	public void insertProcess(Process p, long clock) {
 		cpuQueue.insert(p);
-		if (currentProcess == null) {
-			switchProcess();
+		p.addedToReadyQueue();
+		statistics.insertsCPUQueue++;
+		if (currentProcess == null && cpuQueue.getQueueLength() == 1) {
+			eventQueue.insertEvent(new Event(Simulator.SWITCH_PROCESS, clock));
 		}
 	}
-	
+
 	public void work(long clock) {
 		if (currentProcess != null) {
 			long timeNeeded = currentProcess.getCpuNeeded();
-			if (timeNeeded > maxCpuTime) {
+			long nextIO = currentProcess.getCPUTimeToNextIO();
+			if (timeNeeded > maxCpuTime && nextIO > maxCpuTime) {
 				currentProcess.giveCpuTime(maxCpuTime);
 				eventQueue.insertEvent(new Event(Simulator.SWITCH_PROCESS, clock + maxCpuTime));
-			} else {
+				statistics.cpuTimeSpent += maxCpuTime;
+				statistics.processShifts++;
+			} else if (timeNeeded > nextIO && nextIO <= maxCpuTime) { // we must check if process need IO before it is finsihed
+				currentProcess.giveCpuTime(nextIO);
+				eventQueue.insertEvent(new Event(Simulator.IO_REQUEST, clock + nextIO));
+				eventQueue.insertEvent(new Event(Simulator.SWITCH_PROCESS, clock + nextIO + 1));
+				statistics.cpuTimeSpent += nextIO;
+			} else if (timeNeeded <= maxCpuTime) {
 				currentProcess.giveCpuTime(timeNeeded);
 				eventQueue.insertEvent(new Event(Simulator.END_PROCESS, clock + timeNeeded));
+				eventQueue.insertEvent(new Event(Simulator.SWITCH_PROCESS, clock + timeNeeded + 1));
+				statistics.cpuTimeSpent += timeNeeded;
 			}
 		}
 	}
@@ -48,25 +63,33 @@ public class CPU {
 	public Process endCurrentProcess() {
 		Process endedProcess = null;
 		if (currentProcess != null && currentProcess.getCpuNeeded() <= 0) {
-			deallocate(currentProcess);
-			endedProcess  = currentProcess;
+			endedProcess = currentProcess;
 			currentProcess = null;
+		} else {
+			System.err.println("BUG!");
 		}
+		gui.setCpuActive(null);
 		return endedProcess;
 	}
 
 	public void switchProcess() {
-		if (currentProcess != null) {
-			cpuQueue.insert(currentProcess);
+		if (cpuQueue.getQueueLength() > 0) {
+			if (currentProcess != null) {
+				cpuQueue.insert(currentProcess);
+			}
+			currentProcess = (Process) cpuQueue.removeNext();
+			gui.setCpuActive(currentProcess);
 		}
-		currentProcess = (Process) cpuQueue.removeNext();
-	}
-
-	public void deallocate(Process p) {
-		p.updateStatistics(statistics);
 	}
 
 	public long getMaximumCpuTime() {
 		return maxCpuTime;
+	}
+
+	public Process fetchCurrentProcess() {
+		Process fetched = currentProcess;
+		currentProcess = null;
+		gui.setCpuActive(null);
+		return fetched;
 	}
 }
