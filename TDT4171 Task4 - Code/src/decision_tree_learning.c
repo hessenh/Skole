430ad8e433a5d615a0eed643a49f855586516da4
Stian Hvatum <stian@stian-deb.hvatum.dyndns.org>
2012-03-11 00:57:20 +0100

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: Stian Hvatum <stian@stian-deb.hvatum.dyndns.org>
    
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   TDT4171 Task4 - Code/Makefile
    	modified:   TDT4171 Task4 - Code/bin/tree-learning.exe
    	modified:   TDT4171 Task4 - Code/obj/decision_tree_learning.o
    	modified:   TDT4171 Task4 - Code/obj/importance.o
    	modified:   TDT4171 Task4 - Code/obj/main.o
    	modified:   TDT4171 Task4 - Code/obj/plurality_value.o
    	modified:   TDT4171 Task4 - Code/src/decision_tree_learning.c
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	TDT4171 Task3/.externalToolBuilders/
    	TDT4171 Task3/.texlipse
    	TDT4171 Task3/Makefile
    	TDT4171 Task3/document.d
    	TDT4171 Task3/document.dvi
    	TDT4171 Task3/document.pdf
    	TDT4171 Task3/hvatum_answers.pdf
    	TDT4171 Task3/hvatum_dss_network_xdsl.zip
    	TDT4171 Task3/tmp/document.idx
    	TDT4171 Task3/tmp/document.ilg
    	TDT4171 Task3/tmp/document.ind
    	TDT4171 Task3/tmp/document.out
    	TDT4171 Task3/tmp/document.toc
    	TDT4171 Task4 - Code/example_set.txt
    	TDT4171 Task4 - Code/include/types.h
    	TDT4171 Task4 - Code/new_train
    	TDT4171 Task4 - Code/obj/types.o
    	TDT4171 Task4 - Code/src/types.c
    	TDT4205 Task3/.settings/
    	TDT4205 Task3/hvatum_answers.pdf
    	TDT4205 Task3/hvatum_code.tar.gz
    	TDT4205 Task3/ps3/
    	TDT4205 Task3/tmp/document.idx

--- TDT4171 Task4 - Code/src/decision_tree_learning.c
+++ TDT4171 Task4 - Code/src/decision_tree_learning.c
@@ -14,38 +14,42 @@
 int
 guess_value(node* tree, obj* example)
 {
- if (tree->num_children)
+  if (tree->num_children)
     {
-        for (int i = 0; i < tree->num_children; i++)
+      for (int i = 0; i < tree->num_children; i++)
         {
-            if (example->attribs[tree->attrib] == tree->children[i]->value)
-                return guess_value(tree->children[i], example);
+          if (example->attribs[tree->attrib] == tree->children[i]->value)
+            return guess_value(tree->children[i], example);
         }
     }
-    return tree->attrib;
+  return tree->attrib;
 }
 
 void
 print_subtree(node* root, int indent)
 {
   char space[indent + 1];
-  for (int i = 0; i < indent*3; i++)
+  for (int i = 0; i < indent * 3; i++)
     {
       space[i++] = ' ';
       space[i++] = '|';
-      space[i]   = ' ';
+      space[i] = ' ';
     }
   space[indent] = 0;
   if (root->value)
-      printf("%sVal:%d ", space, root->value);
-  if (root->num_children)
-      printf("Attrib:%d\n", root->attrib + 1);
-  else
-     printf("Class:%d\n", root->attrib);
-
-  for (int i = 0; i < root->num_children; i++)
+    printf("%sVal:%d ", space, root->value);
+  if (!root->num_children)
     {
-      print_subtree(root->children[i], indent + 3);
+      printf("Class:%d\n", root->attrib);
+    }
+  else
+    {
+      printf("Attrib:%d\n", root->attrib + 1);
+
+      for (int i = 0; i < root->num_children; i++)
+        {
+          print_subtree(root->children[i], indent + 3);
+        }
     }
 }
 
@@ -71,17 +75,18 @@ same_class(obj** examples, int num_examples)
 }
 
 int
-get_examples(obj** examples, int num_examples, int attrib_index, int attrib_val, obj*** new_examples)
+get_examples(obj** examples, int num_examples, int attrib_index, int attrib_val,
+    obj*** new_examples)
 {
-  int size = num_examples/2;
+  int size = num_examples / 2;
   int index = 0;
-  *new_examples = malloc(sizeof(obj*)*size);
+  *new_examples = malloc(sizeof(obj*) * size);
   for (int i = 0; i < num_examples; i++)
     {
       if (examples[i]->attribs[attrib_index] == attrib_val)
         {
           if (index >= size)
-            *new_examples = realloc(*new_examples, sizeof(obj*)*(size*=2));
+            *new_examples = realloc(*new_examples, sizeof(obj*) * (size *= 2));
           (*new_examples)[index++] = examples[i];
         }
     }
@@ -97,19 +102,20 @@ print_tree(node* root)
 int*
 remove_attrib(int* attribs, int num_attribs, int remove)
 {
-    int* new_attribs = malloc(sizeof(int)*(num_attribs-1));
-    int j = 0;
-    for (int i = 0; i < num_attribs; i++)
+  int* new_attribs = malloc(sizeof(int) * (num_attribs - 1));
+  int j = 0;
+  for (int i = 0; i < num_attribs; i++)
     {
-        if (attribs[i] != remove)
-            new_attribs[j++] = attribs[i];
+      if (attribs[i] != remove)
+        new_attribs[j++] = attribs[i];
     }
-    return new_attribs;
+  return new_attribs;
 }
 
-node* leaf_node(int attrib, int value)
+node*
+leaf_node(int attrib, int value)
 {
-        return create_node(NULL, 0, attrib, value);
+  return create_node(NULL, 0, attrib, value);
 }
 
 node*
@@ -118,7 +124,8 @@ decision_tree_learning(obj** examples, int num_examples, int* attribs,
 {
   if (num_examples == 0)
     {
-      return leaf_node(plurality_value(parent_examples, num_parent_examples), value);
+      return leaf_node(plurality_value(parent_examples, num_parent_examples),
+          value);
     }
   else if (same_class(examples, num_examples))
     {
@@ -130,21 +137,27 @@ decision_tree_learning(obj** examples, int num_examples, int* attribs,
     }
   else
     {
-      int most_important = importance(examples, num_examples, attribs, num_attribs);
+      int most_important = importance(examples, num_examples, attribs,
+          num_attribs);
       node* learning_tree = malloc(sizeof(node));
       learning_tree->attrib = most_important;
       learning_tree->value = value;
-      learning_tree->children = malloc(sizeof(node*)*2);
+      learning_tree->children = malloc(sizeof(node*) * 2);
       learning_tree->num_children = 0;
       for (int attrib_val = 1; attrib_val < 3; attrib_val++)
         {
-           obj** new_examples;
-           int num_new_examples = get_examples(examples, num_examples, most_important, attrib_val, &new_examples);
+          obj** new_examples;
+          int num_new_examples = get_examples(examples, num_examples,
+              most_important, attrib_val, &new_examples);
 
-           int* new_attribs = remove_attrib(attribs, num_attribs, most_important);
-           learning_tree->children[learning_tree->num_children++] = decision_tree_learning(new_examples, num_new_examples, new_attribs, num_attribs - 1, examples, num_examples, attrib_val);
-           free(new_examples);
-           free(new_attribs);
+          int* new_attribs = remove_attrib(attribs, num_attribs,
+              most_important);
+          learning_tree->children[learning_tree->num_children++] =
+              decision_tree_learning(new_examples, num_new_examples,
+                  new_attribs, num_attribs - 1, examples, num_examples,
+                  attrib_val);
+          free(new_examples);
+          free(new_attribs);
         }
       return learning_tree;
     }
@@ -153,12 +166,12 @@ decision_tree_learning(obj** examples, int num_examples, int* attribs,
 void
 clean_tree(node* tree)
 {
- if (tree->children)
- {
-    for (int i = 0; i < tree->num_children; i++)
+  if (tree->children)
+    {
+      for (int i = 0; i < tree->num_children; i++)
         if (tree->children[i])
-            clean_tree(tree->children[i]);
-    free(tree->children);
- }
- free(tree);
+          clean_tree(tree->children[i]);
+      free(tree->children);
+    }
+  free(tree);
 }
