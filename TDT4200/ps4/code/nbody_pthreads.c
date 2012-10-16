b1ed79271393071c49e8b55b82bfc39302cc9a51
Stian Hvatum <stian@hvatum.net>
2012-10-16 23:53:11 +0200

    Why u no work, pthreads

--- TDT4200/ps4/code/nbody_pthreads.c
+++ TDT4200/ps4/code/nbody_pthreads.c
@@ -26,9 +26,29 @@ int num_planets;
 int num_timesteps;
 int output;
 
+int portion;
+vec2** local_forces;
+int counter;
+pthread_mutex_t mutex;
+pthread_cond_t cond_var;
+
 vec2* forces;
 planet* planets;
 
+void barrier() {
+    pthread_mutex_lock(&mutex);
+    counter++;
+    if (counter == num_threads)
+    {
+        counter = 0;
+        pthread_cond_broadcast(&cond_var);
+    }
+    else
+    {
+        while (pthread_cond_wait(&cond_var, &mutex) != 0);
+    }
+    pthread_mutex_unlock(&mutex);
+}
 
 // Parse command line arguments
 void parse_args(int argc, char** argv){
@@ -48,7 +68,6 @@ void parse_args(int argc, char** argv){
 // Reads planets from planets.txt
 void read_planets(){
 
-    char* a;
     FILE* file = fopen("planets.txt", "r");
     if(file == NULL){
         printf("'planets.txt' not found. Exiting\n");
@@ -56,13 +75,13 @@ void read_planets(){
     }
 
     char line[200];
-    a = fgets(line, 200, file);
+    fgets(line, 200, file);
     sscanf(line, "%d", &num_planets);
 
     planets = (planet*)malloc(sizeof(planet)*num_planets);
 
     for(int p = 0; p < num_planets; p++){
-        a = fgets(line, 200, file);
+        fgets(line, 200, file);
         sscanf(line, "%lf %lf %lf %lf %lf",
                 &planets[p].position.x,
                 &planets[p].position.y,
@@ -78,10 +97,10 @@ void read_planets(){
 void write_planets(int timestep, int output){
     char name[20];
     if(output == 1){
-        int n = sprintf(name, "%04d.dat", timestep);
+        sprintf(name, "%04d.dat", timestep);
     }
     else{
-        int n = sprintf(name, "planets_out.txt");
+        sprintf(name, "planets_out.txt");
     }
 
     FILE* file = fopen(name, "wr+");
@@ -124,44 +143,66 @@ vec2 compute_force(planet p, planet q){
 
 void* loop(void* arg)
 {
-    
+    // Calculate what to calculate :D
+    int rank = (long)arg;
+    int start_p = portion * rank;
+    int end_p;
+    if (num_planets >= portion + start_p)
+        end_p = portion + start_p;
+    else
+        end_p = num_planets;
+
+    printf("Hello from rank %d, starting at %d, ending at %d\n", rank, start_p, end_p);
+
     // Main loop
     for(int t = 0; t < num_timesteps; t++){
-      if(output == 1){
+        if(output == 1 && rank == 0){
             write_planets(t, 1);
         }
-        
-        // Clear forces
-        for(int i = 0; i < num_planets; i++){
+        barrier();
+
+        /* Clear forces
+        for(int i = start_p; i < end_p; i++){
             forces[i].x = 0;
             forces[i].y = 0;
         }
-        
+        */
+
         // Update forces
-        for(int p = 0; p < num_planets; p++){
+        for(int p = start_p; p < end_p; p++){
             for(int q = p+1; q < num_planets; q++){
                 vec2 f = compute_force(planets[p], planets[q]);
 
-                forces[p].x += f.x;
-                forces[p].y += f.y;
+                local_forces[rank][p].x += f.x;
+                local_forces[rank][p].y += f.y;
 
-                forces[q].x -= f.x;
-                forces[q].y -= f.y;
+                local_forces[rank][q].x -= f.x;
+                local_forces[rank][q].y -= f.y;
             }
         }
+        barrier();
 
+        // Apply updated forces
+        for(int p = start_p; p < end_p; p++)
+        {
+            forces[p].x = forces[p].y = 0;
+            for (int i = 0; i < num_threads; i++)
+            {
+                forces[p].x += local_forces[i][p].x;
+                forces[p].y += local_forces[i][p].y;
+            }
+        }
+        barrier();
         // Update positions and velocities
-        for(int p = 0; p < num_planets; p++){
+        for(int p = start_p; p < end_p; p++){
             planets[p].position.x += dT * planets[p].velocity.x;
             planets[p].position.y += dT * planets[p].velocity.y;
 
             planets[p].velocity.x += dT * forces[p].x / planets[p].mass;
             planets[p].velocity.y += dT * forces[p].y / planets[p].mass;
         }
-
-      }
-
-
+    }
+    return NULL;
 }
 
 int main(int argc, char** argv){
@@ -171,7 +212,31 @@ int main(int argc, char** argv){
     read_planets();
 
     forces = (vec2*)malloc(sizeof(vec2)*num_planets);
+
+    local_forces = (vec2**)malloc(sizeof(vec2*)*num_threads);
+    for (int i = 0; i < num_threads; i++)
+    {
+        local_forces[i] = (vec2*)malloc(sizeof(vec2)*num_planets);
+        for (int p = 0; p < num_planets; p++)
+            local_forces[i][p].x = local_forces[i][p].y = 0;
+    }
+
+    pthread_t threads[num_threads-1];
+    portion = num_planets / num_threads + 1;
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cond_var,NULL);
+
+    for (int i = 0; i < num_threads-1; i++)
+    {
+        pthread_create(&threads[i], NULL, loop, (void*)((long)(i+1)));
+    }
     loop(NULL);
+
+    for (int i = 0; i < num_threads-1; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
 
     if(output == 0){
         write_planets(num_timesteps,0);
