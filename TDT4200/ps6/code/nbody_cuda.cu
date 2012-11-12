0cc087ea500729345d7f10ec3f5c2e773a96cbf9
Stian Hvatum <hvatum@nvda-lab-00>
2012-11-12 13:57:32 +0100

    Changed some arithmetics to get more correct result

--- TDT4200/ps6/code/nbody_cuda.cu
+++ TDT4200/ps6/code/nbody_cuda.cu
@@ -6,7 +6,7 @@
 
 #define dT 0.2f
 #define G 0.6f
-#define BLOCK_SIZE 64
+#define BLOCK_SIZE 32
 
 // Global variables
 int num_planets;
@@ -19,7 +19,6 @@ float4* planets;
 // Device arrays 
 float2* velocities_d;
 float4* planets_d;
-
 
 // Parse command line arguments
 void parse_args(int argc, char** argv){
@@ -81,22 +80,68 @@ void write_planets(int timestep){
 
 // TODO 7. Calculate the change in velocity for p, caused by the interaction with q
 __device__ float2 calculate_velocity_change_planet(float4 p, float4 q){
+    float2 dv;
+    float2 dist;
+    dist.x = q.x - p.x;
+    dist.y = q.y - p.y;
 
+
+    float abs_dist = sqrt(dist.x*dist.x + dist.y*dist.y);
+    float dist_cubed = abs_dist*abs_dist*abs_dist;
+    //printf("%f %f\n", abs_dist, dist_cubed);
+
+
+
+    dv.x = dT*G*q.z/dist_cubed * dist.x;
+    dv.y = dT*G*q.z/dist_cubed * dist.y;
+    return dv;
 }
 
 // TODO 5. Calculate the change in velocity for my_planet, caused by the interactions with a block of planets
 __device__ float2 calculate_velocity_change_block(float4 my_planet, float4* shared_planets){
+    float2 velocityChange;
+    velocityChange.x = 0;
+    velocityChange.y = 0;
 
+    for (int i = 0; i < BLOCK_SIZE; i++)
+    {
+        if (my_planet.x == shared_planets[i].x && my_planet.y == shared_planets[i].y) continue;
+        float2 newChange = calculate_velocity_change_planet(my_planet, shared_planets[i]);
+        velocityChange.x += newChange.x;
+        velocityChange.y += newChange.y;
+    }
+
+    return velocityChange;
 }
 
 // TODO 4. Update the velocities by calculating the planet interactions
 __global__ void update_velocities(float4* planets, float2* velocities, int num_planets){
+    int tid = threadIdx.x + blockIdx.x*blockDim.x;
 
+    __shared__ float4 shared[BLOCK_SIZE];
+    float4 planet = planets[tid];
+    float2 velocityChange;
+    velocityChange.x = velocities[tid].x;
+    velocityChange.y = velocities[tid].y;
+
+    for (int i = 0; i < num_planets; i+=BLOCK_SIZE)
+    {
+        shared[threadIdx.x] = planets[threadIdx.x + i];
+        __syncthreads();
+        float2 vc = calculate_velocity_change_block(planet, shared);
+        velocityChange.x += vc.x;
+        velocityChange.y += vc.y;
+        __syncthreads();
+    }
+    velocities[tid].x = velocityChange.x;
+    velocities[tid].y = velocityChange.y;
 }
 
 // TODO 7. Update the positions of the planets using the new velocities
 __global__ void update_positions(float4* planets, float2* velocities, int num_planets){
-
+    int tid = threadIdx.x + blockIdx.x*blockDim.x;
+    planets[tid].x += velocities[tid].x * dT;
+    planets[tid].y += velocities[tid].y * dT;
 }
 
 
@@ -129,7 +174,7 @@ int main(int argc, char** argv){
     /* and check for errors */
     if (error != cudaSuccess)
         printf("Copy to velocities to device: %d\n", error);
-  
+
     // Calculating the number of blocks
     int num_blocks = num_planets/BLOCK_SIZE + ((num_planets%BLOCK_SIZE == 0) ? 0 : 1);
 
@@ -141,12 +186,13 @@ int main(int argc, char** argv){
         error = cudaGetLastError();
         if (error != cudaSuccess)
             printf("update_velocities error - Step: %d CUDA Code: %d\n", t, error);
-
+        cudaThreadSynchronize();
         update_positions<<<num_blocks,BLOCK_SIZE>>>(planets_d, velocities_d, num_planets);
         // We print the error code from last call if things went bad
         error = cudaGetLastError();
         if (error != cudaSuccess)
             printf("update_positions errror - Step: %d CUDA Code: %d\n", t, error);
+        cudaThreadSynchronize();
 
     }
 
